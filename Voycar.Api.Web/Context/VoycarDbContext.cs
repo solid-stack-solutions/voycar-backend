e25ae4721a20bee3697ac88aa0617ce1ba34e803
kevin <kevin@DESKTOP-D35OFOB>
2024-05-25 21:53:41 +0200

    feat: add Member entity and DbSet to VoycarDbContext.cs

--- Voycar.Api.Web/Context/VoycarDbContext.cs
+++ Voycar.Api.Web/Context/VoycarDbContext.cs
@@ -1,9 +1,11 @@
 namespace Voycar.Api.Web.Context;
 
+using Entities;
 using Microsoft.EntityFrameworkCore;
 
 public class VoycarDbContext : DbContext
 {
     public VoycarDbContext(DbContextOptions<VoycarDbContext> options) : base(options){ }
 
+    public DbSet<Member> Members { get; set; }
 }
---
+++ Voycar.Api.Web/Entities/Member.cs
@@ -0,0 +1,39 @@
+namespace Voycar.Api.Web.Entities;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Member
+{
+    public int Id { get; set; }
+
+    // Login
+    [EmailAddress]
+    public required string Email{ get; set; }
+    public string PasswordHash { get; set; }
+
+    // Personal Details
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Street { get; set; }
+    public required string HouseNumber { get; set; }
+    public required string PostalCode { get; set; }
+    public required string Place { get; set; }
+    public required DateTime BirthDate { get; set; }
+    public required string BirthPlace { get; set; }
+    public required string PhoneNumber { get; set; }
+    public int RoleId { get; set; }
+    public int TariffId { get; set; }
+    public required string? DriversLicenseNumber { get; set; }
+    public bool ValidDriversLicense { get; set; }
+    public required string IdCardNumber { get; set; }
+    public bool ValidPostIdent { get; set; }
+
+
+    // Verification
+    public string? VerificationToken { get; set; }
+    public DateTime? VerifiedAt { get; set; }
+
+    // Reset
+    public string? PasswordResetToken { get; set; }
+    public DateTime? ResetTokenExpires { get; set; }
+}
