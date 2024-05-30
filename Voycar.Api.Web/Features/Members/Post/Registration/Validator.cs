9a9fc6aedc36c0d8bc3d016cc778e63fada5e10e
kvnlnk <klingk.msg@gmail.com>
2024-05-30 16:04:17 +0200

    refac: change order of Validator.cs

--- Voycar.Api.Web/Features/Members/Post/Registration/Validator.cs
+++ Voycar.Api.Web/Features/Members/Post/Registration/Validator.cs
@@ -14,51 +14,39 @@ public class Validator : Validator<Request>
             .NotEmpty().WithMessage("your email is required!")
             .EmailAddress().WithMessage("not a valid email address");
 
-
         this.RuleFor(member => member.FirstName)
             .NotEmpty().WithMessage("your firstname is required!")
             .Length(2, 250).WithMessage("your name is too short!");
-
 
         this.RuleFor(member => member.LastName)
             .NotEmpty().WithMessage("your firstname is required!")
             .Length(2, 250).WithMessage("your name is too short!");
 
+        this.RuleFor(member => member.Street)
+            .NotEmpty().WithMessage("your street is required!");
+
+        this.RuleFor(member => member.HouseNumber)
+            .NotEmpty().WithMessage("your house number is required!");
+
+        this.RuleFor(member => member.PostalCode)
+            .NotEmpty().WithMessage("your postal code is required!");
+
+        this.RuleFor(member => member.Place)
+            .NotEmpty().WithMessage("your place is required!");
 
         this.RuleFor(member
                 => (DateOnly.FromDateTime(DateTime.UtcNow).DayNumber
                     - member.BirthDate.DayNumber) / 365)
             .GreaterThan(18).WithMessage("you are not legal yet!");
 
+        this.RuleFor(member => member.BirthPlace)
+            .NotEmpty().WithMessage("your birth place is required!");
 
         this.RuleFor(member => member.PhoneNumber)
             .NotEmpty().WithMessage("your phone number is required!");
 
-
-        this.RuleFor(member => member.Street)
-            .NotEmpty().WithMessage("your street is required!");
-
-
-        this.RuleFor(member => member.HouseNumber)
-            .NotEmpty().WithMessage("your house number is required!");
-
-
-
-        this.RuleFor(member => member.PostalCode)
-            .NotEmpty().WithMessage("your postal code is required!");
-
-
-        this.RuleFor(member => member.Place)
-            .NotEmpty().WithMessage("your place is required!");
-
-
-        this.RuleFor(member => member.BirthPlace)
-            .NotEmpty().WithMessage("your birth place is required!");
-
-
         this.RuleFor(member => member.DriversLicenseNumber)
             .NotEmpty().WithMessage("your DriversLicenseNumber is required!");
-
 
         this.RuleFor(member => member.IdCardNumber)
             .NotEmpty().WithMessage("your IdCardNumber is required!");
