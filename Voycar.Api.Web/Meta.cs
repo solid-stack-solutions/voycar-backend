76592cb9d0d2f0cc5cff700a7e6eaeefdaf9e8e5
kenoalpe <git@kenoalbers.de>
2024-05-31 20:27:51 +0200

    refac: add global using for httpResults to meta.cs

--- Voycar.Api.Web/Meta.cs
+++ Voycar.Api.Web/Meta.cs
@@ -3,5 +3,7 @@ global using Serilog;
 global using FastEndpoints;
 global using FastEndpoints.Swagger;
 
+global using Microsoft.AspNetCore.Http.HttpResults;
+
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Tests")]
