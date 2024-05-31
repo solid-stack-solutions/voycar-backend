6a7db644690f6df76c3907031d52cd3542615350
kenoalpe <git@kenoalbers.de>
2024-05-31 20:14:02 +0200

    feat: generic single endpoints for post and get

--- Voycar.Api.Web/Generic/Endpoint/Get/Single/Single.cs
+++
@@ -1,9 +0,0 @@
-namespace Voycar.Api.Web.Generic.Endpoint.Get.Single;
-
-using Microsoft.AspNetCore.Http.HttpResults;
-
-public abstract class Single<TEntity, TMapper>
-    : Endpoint<Request, Results<Ok<Response<TEntity>>, NotFound>>, TMapper>
-    where TMapper : ResponseMapper<Ok<>,>
-{
-}
--- Voycar.Api.Web/Generic/Endpoint/Post/Single.cs
+++ Voycar.Api.Web/Generic/Endpoint/Post/Single.cs
@@ -3,32 +3,34 @@ namespace Voycar.Api.Web.Generic.Endpoint.Post;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Repository;
 
-public abstract class Single<TEntity, TRequest, TMapper>
-    : Endpoint<TRequest, Results<Ok, NoContent>, TMapper>
+public abstract class Single<TEntity>
+    : Endpoint<TEntity, Results<Ok, NoContent>>
     where TEntity : Entity
-    where TRequest : class
-    where TMapper : RequestMapper<TRequest, TEntity>
 {
     private readonly IRepository<TEntity> _repository;
-    private readonly string route;
     private readonly string[] roles;
 
-    protected Single(IRepository<TEntity> repository, string route, string[] roles)
+    protected Single(IRepository<TEntity> repository, string[] roles)
     {
         this._repository = repository;
-        this.route = route;
         this.roles = roles;
     }
 
     public override void Configure()
     {
-        this.Post(this.route);
+        this.Post(typeof(TEntity).Name);
         this.Roles(this.roles);
     }
 
-    public override async Task HandleAsync(TRequest req, CancellationToken ct)
+    public override async Task HandleAsync(TEntity req, CancellationToken ct)
     {
-        var created = this._repository.Create(await this.Map.ToEntityAsync(req, ct));
-        await this.SendResultAsync(created ? TypedResults.Ok() : TypedResults.NoContent());
+        var created = this._repository.Create(req);
+
+        if (created)
+        {
+            await this.SendResultAsync(TypedResults.Ok());
+        }
+
+        await this.SendResultAsync(TypedResults.NoContent());
     }
 }
