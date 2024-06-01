1beb1b0253393001f8bf5362cb6a44e6cafa47de
kevin-lingk <klingk.msg@gmail.com>
2024-06-01 19:49:45 +0200

    feat: add modelbuilder

--- Voycar.Api.Web/Context/VoycarDbContext.cs
+++ Voycar.Api.Web/Context/VoycarDbContext.cs
@@ -8,6 +8,27 @@ public class VoycarDbContext : DbContext
     public VoycarDbContext(DbContextOptions<VoycarDbContext> options) : base(options) {}
 
     // db sets for entities
+    public DbSet<User> Users { get; set; }
     public DbSet<Member> Members { get; set; }
-    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure the one-to-one relationship between User and Member
+        modelBuilder.Entity<Member>()
+            .HasOne(m => m.User)
+            .WithOne()
+            .HasForeignKey<Member>(m => m.UserId)
+            .IsRequired();
+
+        // Configure the one-to-many relationship between Role and User
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Role)
+            .WithMany()
+            .HasForeignKey(u => u.RoleId)
+            .IsRequired();
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
