c4096487d3cc45e40fd13a5c411c9bc4bef3fd69
kenoalpe <git@kenoalbers.de>
2024-06-02 16:46:49 +0200

    feat: test against PostgresDb setup and example test

--- Voycar.Api.Web.Tests/Features/Roles/Tests.cs
+++ Voycar.Api.Web.Tests/Features/Roles/Tests.cs
@@ -1,20 +1,21 @@
 namespace Voycar.Api.Web.Tests.Features.Roles;
 
-using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
 using Context;
 using Entities;
-using Moq;
-using Web.Features.Roles.Repository;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Web.Context;
 using Web.Features.Roles.Endpoints.Post;
+using Web.Features.Roles.Repository;
 
-public class Tests(App app) : TestBase<App>
+public class Tests(Sut app) : TestBase<Sut>
 {
-    // TODO add tests and remove example
     [Fact]
     public async Task ExamplePost()
     {
         var httpResponseMessage = await app.Client.POSTAsync<Endpoint, Role>(new Role() { Name = "Hallo" });
 
-        httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
