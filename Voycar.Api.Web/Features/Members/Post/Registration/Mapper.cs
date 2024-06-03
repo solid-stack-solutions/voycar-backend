3cb840c2edd43826a9cb979a57a5982724eeceaf
kevin-lingk <klingk.msg@gmail.com>
2024-06-03 23:41:48 +0200

    feat: add new way to retrieve role id

--- Voycar.Api.Web/Features/Members/Post/Registration/Mapper.cs
+++ Voycar.Api.Web/Features/Members/Post/Registration/Mapper.cs
@@ -2,9 +2,16 @@ namespace Voycar.Api.Web.Features.Members.Post.Registration;
 
 using System.Security.Cryptography;
 using Entities;
+using Roles.Repository;
 
 public class Mapper : Mapper<Request, Response, Member>
 {
+    private readonly IRoles _roleRepository;
+
+    public Mapper(IRoles roleRepository)
+    {
+        this._roleRepository = roleRepository;
+    }
 
     public override Member ToEntity(Request r)
     {
@@ -12,7 +19,7 @@ public class Mapper : Mapper<Request, Response, Member>
         var user = new User
         {
             Id = Guid.NewGuid(),
-            Email = r.Email,
+            Email = r.Email.ToLowerInvariant(),
             PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(r.Password),
             FirstName = r.FirstName,
             LastName = r.LastName,
@@ -20,13 +27,14 @@ public class Mapper : Mapper<Request, Response, Member>
             HouseNumber = r.HouseNumber,
             PostalCode = r.PostalCode,
             City = r.City,
-            RoleId = 1 // Sets RoleId to member as default
+            // Retrieve the RoleId for the role named "member".
+            RoleId = this._roleRepository.Retrieve("member").Result!.Id
         };
 
         // Create the Member entity and link it to the User entity
         var member = new Member
         {
-            UserId = user.Id, // Set the foreign key to the User's Id
+            Id = user.Id, // Set the foreign key to the User's Id
             User = user,      // Set the navigation property, useful for Repository
             BirthDate = r.BirthDate,
             BirthPlace = r.BirthPlace,
