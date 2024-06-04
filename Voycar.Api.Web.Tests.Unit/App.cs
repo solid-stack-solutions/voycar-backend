6683afa803fd80079b3ce8a189cf0639ea8a2830
kenoalpe <git@kenoalbers.de>
2024-06-04 17:58:13 +0200

    style: capitalise commend letters

--- Voycar.Api.Web.Tests.Unit/App.cs
+++ Voycar.Api.Web.Tests.Unit/App.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
+namespace Voycar.Api.Web.Tests.Unit;
+
 public class App : AppFixture<Program>
 {
     protected override Task SetupAsync()
     {
-        // place one-time setup code here
+        // Place one-time setup code here
         return Task.CompletedTask;
     }
 
     protected override void ConfigureApp(IWebHostBuilder a)
     {
-        // do host builder config here
+        // Do host builder config here
     }
 
     protected override void ConfigureServices(IServiceCollection s)
     {
-        // do test service registration here
+        // Do test service registration here
     }
 
     protected override Task TearDownAsync()
     {
-        // do cleanups here
+        // Do cleanups here
         return Task.CompletedTask;
     }
 }
