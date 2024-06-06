fdb7332c3b2ae9124c23f451c24c6293fc13215b
Jannik <jannik.hennicke@gmail.com>
2024-06-06 20:50:56 +0200

    doc: add description and summary to generic DELETE Endpoint

--- Voycar.Api.Web/Generic/Endpoint/Delete/Single.cs
+++ Voycar.Api.Web/Generic/Endpoint/Delete/Single.cs
@@ -19,6 +19,21 @@ public abstract class Single<TEntity>
     {
         this.Delete(typeof(TEntity).Name.ToLowerInvariant() + "/{id}");
         this.Roles(this.roles);
+        Description(b => b
+                .Accepts<Entity>("Voycar.Api.Web/Generic/Entity")
+                .Produces<IResult>(200)
+                .ProducesProblem(404),
+            clearDefaults: true);
+        Summary(s =>
+        {
+            s.Summary = $"DELETE Endpoint for {typeof(TEntity).Name}";
+            s.Description = $"This Endpoint is used to remove {typeof(TEntity).Name} objects from the database";
+            s.Responses[200] = "OK response if DELETE operation was successful";
+            s.Responses[404] =
+                "Not Found response if DELETE operation was performed for on an Entity that could not be found in the database";
+            s.ResponseExamples[404] = new {};
+            s.Params["id"] = $"id derived from the {typeof(TEntity).Name} object";
+        });
     }
 
     public override async Task HandleAsync(Entity req, CancellationToken ct)
