b71c635ef1f1ff893d8b3ed3b8a6f47e120b7a5e
Jannik <jannik.hennicke@gmail.com>
2024-06-06 20:51:06 +0200

    doc: add description and summary to generic GET Endpoint

--- Voycar.Api.Web/Generic/Endpoint/Get/Single.cs
+++ Voycar.Api.Web/Generic/Endpoint/Get/Single.cs
@@ -19,6 +19,22 @@ public abstract class Single<TEntity>
     {
         this.Get(typeof(TEntity).Name.ToLowerInvariant() + "/{id}");
         this.Roles(this.roles);
+        Description(b => b
+                .Accepts<Entity>("Voycar.Api.Web/Generic/Entity")
+                .Produces<TEntity>(200)
+                .ProducesProblem(404),
+            clearDefaults: true);
+        Summary(s =>
+        {
+            s.Summary = $"GET Endpoint for {typeof(TEntity).Name}";
+            s.Description =
+                $"This Endpoint is used to retrieve a {typeof(TEntity).Name} object from the database";
+            s.Responses[200] = $"{typeof(TEntity).Name} object and OK response if GET operation was successful";
+            s.Responses[404] =
+                "Not Found response if GET operation was performed for on an Entity that could not be found in the database";
+            s.ResponseExamples[404] = new {};
+            s.Params["id"] = $"id derived from the {typeof(TEntity).Name} object";
+        });
     }
 
     public override async Task HandleAsync(Entity req, CancellationToken ct)
