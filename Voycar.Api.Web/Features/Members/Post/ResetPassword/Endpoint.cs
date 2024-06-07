67bf32d9240802e6861b0c848f4d32f556c6a4b2
kevin-lingk <klingk.msg@gmail.com>
2024-06-07 21:58:52 +0200

    refac: change route and add user repository

--- Voycar.Api.Web/Features/Members/Post/ResetPassword/Endpoint.cs
+++ Voycar.Api.Web/Features/Members/Post/ResetPassword/Endpoint.cs
@@ -1,30 +1,30 @@
 namespace Voycar.Api.Web.Features.Members.Post.ResetPassword;
 
-using System.Security.Cryptography;
 using Repository;
-using Services.EmailService;
 
-// todo
 public class Endpoint : Endpoint<Request>
 {
-    private readonly IMemberRepository _memberRepository;
+    private readonly IUsers _repository;
     private readonly ILogger<Get.Verify.Endpoint> _logger;
 
-    public Endpoint(IMemberRepository memberRepository, ILogger<Get.Verify.Endpoint> logger)
+    public Endpoint(IUsers repository, ILogger<Get.Verify.Endpoint> logger)
     {
-        this._memberRepository = memberRepository;
+        this._repository = repository;
         this._logger = logger;
     }
+
+
     public override void Configure()
     {
-        this.Post("/api/reset-password");
+        this.Post("/reset-password");
         this.AllowAnonymous();
     }
 
+
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        // checks whether there is a user for the req
-        var user = await this._memberRepository.GetPrtAsync(req.PasswordResetToken);
+        // Checks whether there is a user for the req
+        var user = await this._repository.Retrieve("passwordResetToken", req.PasswordResetToken);
 
         if (user is null || user.ResetTokenExpires < DateTime.Now)
         {
@@ -32,15 +32,14 @@ public class Endpoint : Endpoint<Request>
             return;
         }
 
-        // set new password hash
+        // Set new password hash
         user.PasswordHash =  BCrypt.Net.BCrypt.EnhancedHashPassword(req.Password);
 
-        // remove reset options
+        // Remove reset options
         user.ResetTokenExpires = null;
         user.PasswordResetToken = null;
 
-        await this._memberRepository.SaveAsync();
-
+        this._repository.Update(user);
 
         this._logger.LogInformation("Password-Reset-Token successfully created for User with ID: {UserId}", user.Id);
         await this.SendOkAsync(cancellation: ct);
