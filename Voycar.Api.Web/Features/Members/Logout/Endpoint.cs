e3f53bb3180da643cb972a9b1d05b1aa9fc2d36d
kevin-lingk <klingk.msg@gmail.com>
2024-06-07 23:32:51 +0200

    feat: add logout Endpoint.cs

---
+++ Voycar.Api.Web/Features/Members/Logout/Endpoint.cs
@@ -0,0 +1,20 @@
+namespace Voycar.Api.Web.Features.Members.Logout;
+
+using FastEndpoints.Security;
+
+
+public class Endpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        this.Post("/logout");
+        this.AllowAnonymous();
+    }
+
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await CookieAuth.SignOutAsync();
+        await this.SendOkAsync(cancellation: ct);
+    }
+}
