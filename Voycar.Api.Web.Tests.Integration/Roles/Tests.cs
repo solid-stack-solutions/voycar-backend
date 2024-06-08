b152f286c1427529722c105ed8edfd92a120bff5
Janniix <jannik.hennicke@gmail.com>
2024-06-09 01:33:21 +0200

    test: add GET Test for Role

--- Voycar.Api.Web.Tests.Integration/Roles/Tests.cs
+++ Voycar.Api.Web.Tests.Integration/Roles/Tests.cs
@@ -13,6 +13,7 @@ public class Tests : TestBase<App>
     private readonly App _app;
     private readonly VoycarDbContext _context;
 
+    private Guid id;
     // Setup request client
     public Tests(App app)
     {
@@ -37,6 +38,7 @@ public class Tests : TestBase<App>
         const string requestName = "JuNiJa(Ke)²";
         var roleRequestData = new Role { Name = requestName };
 
+        id = roleRequestData.Id; //pass down id to be used in other test
         // Act
         var httpResponseMessage = await this._app.Client.POSTAsync<R.Post.SingleUnique, Role>(roleRequestData);
         var roleInDb = await this._context.Roles.FirstOrDefaultAsync(r => r.Name == roleRequestData.Name);
@@ -44,5 +46,17 @@ public class Tests : TestBase<App>
         // Assert
         httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
         roleInDb.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetIsValid()
+    {
+        const string requestName = "JuNiJa(Ke)²";
+        var role = new Role { Name = requestName };
+        role.Id = this.id;
+        var roleRequestData = role;
+        var httpResponseMessage = await this._app.Client.GETAsync<R.Get.Single, Role>(roleRequestData);
+
+        httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
