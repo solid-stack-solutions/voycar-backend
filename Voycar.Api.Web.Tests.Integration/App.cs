0e5ac644ea1e2af0f8c98f1fb99b3cb51bbd04b8
NilsPvR <nilspvr@pm.me>
2024-06-09 11:41:16 +0200

    refac: move db container creation into PreSetupAsync additionally restructure how the connection string is handled

--- Voycar.Api.Web.Tests.Integration/App.cs
+++ Voycar.Api.Web.Tests.Integration/App.cs
@@ -10,35 +10,47 @@ using Testcontainers.PostgreSql;
 
 public class App : AppFixture<Program>
 {
-    // Use this to test with prod Db, see ConfigureServices()
-    private const string ConnectionString =
-        "User ID=admin;Password=admin;Server=localhost;Port=5432;Database=VoycarDb;";
-
-    // Create a Db test container to run tests against
-    private readonly PostgreSqlContainer _container = new PostgreSqlBuilder()
-        .WithImage("postgres:16.3")
-        .WithDatabase("pgsql-testcontainer")
-        .WithUsername("admin")
-        .WithPassword("admin")
-        .WithExposedPort(5000)
-        .Build();
-
+    /* Change this constant to true, to test against production database
+    WARNING: This will DELETE your existing data */
+    private const bool TestAgainstProductionDb = false;
+    private PostgreSqlContainer Container { get; set; }
+    private string ConnectionString { get; set; }
     public VoycarDbContext Context { get; private set; }
     public HttpClient Member { get; private set; }
     public HttpClient Employee { get; private set; }
     public HttpClient Admin { get; private set; }
 
     // See: https://gist.github.com/dj-nitehawk/04a78cea10f2239eb81c958c52ec84e0
-    protected override Task PreSetupAsync()
+    protected override async Task PreSetupAsync()
     {
-        return this._container.StartAsync();
+        if (TestAgainstProductionDb)
+        {
+            // WARNING: This will test against production database and DELETE existing data
+            this.ConnectionString =
+                "User ID=admin;Password=admin;Server=localhost;Port=5432;Database=VoycarDb;";
+            return;
+        }
+
+        // Create a Db test container to run tests against
+        this.Container =  new PostgreSqlBuilder()
+            .WithImage("postgres:16.3")
+            .WithDatabase("pgsql-testcontainer")
+            .WithUsername("admin")
+            .WithPassword("admin")
+            .WithExposedPort(5000)
+            .Build();
+         await this.Container.StartAsync();
+         this.ConnectionString = this.Container.GetConnectionString();
     }
 
     protected override async Task SetupAsync()
     {
         // == Place one-time setup code here ==
 
-        // Db setup
+
+
+
+        // Set up Db for tests
         this.Context = this.Services.GetRequiredService<VoycarDbContext>();
         await this.Context.Database.EnsureDeletedAsync(); // Delete data on Db
         await this.Context.Database.EnsureCreatedAsync(); // Populate with Db schema
@@ -64,9 +76,7 @@ public class App : AppFixture<Program>
 
         services.AddDbContext<VoycarDbContext>(options =>
         {
-            // HINT: Change ConnectionString here to test against prod-database
-            // WARNING: This WILL delete your existing data
-            options.UseNpgsql(this._container.GetConnectionString());
+            options.UseNpgsql(this.ConnectionString);
         });
 
 
@@ -79,6 +89,6 @@ public class App : AppFixture<Program>
         this.Member.Dispose();
         this.Employee.Dispose();
         this.Admin.Dispose();
-        return this._container.StopAsync();
+        return this.Container.StopAsync();
     }
 }
