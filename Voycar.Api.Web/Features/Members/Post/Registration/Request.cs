ac9bf0635b210811c73e0a8dcb4634fe7660ef03
Nils van Rijsinge <nilspvr@pm.me>
2024-06-11 20:27:08 +0200

    doc: apply suggestions from code review
    change capitalization
    
    Co-authored-by: Kevin <136514166+kvnlnk@users.noreply.github.com>

--- Voycar.Api.Web/Features/Members/Post/Registration/Request.cs
+++ Voycar.Api.Web/Features/Members/Post/Registration/Request.cs
@@ -5,12 +5,12 @@ using System.ComponentModel.DataAnnotations;
 
 public class Request
 {
-    // User Information
+    // User information
     [EmailAddress]
     public string Email { get; set; }
     public string Password { get; set; }
 
-    // Member Information
+    // Member information
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Street { get; set; }
