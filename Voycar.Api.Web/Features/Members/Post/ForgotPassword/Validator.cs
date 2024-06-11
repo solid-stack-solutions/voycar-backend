96be115f660d616d4c29582cb826711e5fa82f75
NilsPvR <nilspvr@pm.me>
2024-06-11 12:10:20 +0200

    fix: validate forgot password request and not login request

--- Voycar.Api.Web/Features/Members/Post/ForgotPassword/Validator.cs
+++ Voycar.Api.Web/Features/Members/Post/ForgotPassword/Validator.cs
@@ -1,14 +1,11 @@
 namespace Voycar.Api.Web.Features.Members.Post.ForgotPassword;
 
-public class Validator : Validator<Login.Request>
+public class Validator : Validator<Request>
 {
     public Validator()
     {
         this.RuleFor(request => request.Email)
             .NotEmpty().WithMessage("your email is required!")
             .EmailAddress().WithMessage("not a valid email address");
-
-        this.RuleFor(request => request.Password)
-            .NotEmpty().WithMessage("your password is required!");
     }
 }
