3a49e83febfa6a0aaee4d29432d272fcc3ea128d
julius-boettger <julius.btg@proton.me>
2024-06-11 11:46:52 +0200

    feat: implement crud endpoints for car feature

--- Voycar.Api.Web/Program.cs
+++ Voycar.Api.Web/Program.cs
@@ -2,6 +2,7 @@ using FastEndpoints.Security;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Voycar.Api.Web.Context;
+using Voycar.Api.Web.Features.Cars.Repository;
 using Voycar.Api.Web.Features.Roles.Repository;
 using Voycar.Api.Web.Features.Members.Repository;
 using Voycar.Api.Web.Features.Members.Services.EmailService;
@@ -56,6 +57,7 @@ try
     builder.Services.AddTransient<IRoles, Roles>();
     builder.Services.AddTransient<IMembers, Members>();
     builder.Services.AddTransient<IUsers, Users>();
+    builder.Services.AddTransient<ICars, Cars>();
 
     // Services
     builder.Services.AddTransient<IEmailService, EmailService>();
