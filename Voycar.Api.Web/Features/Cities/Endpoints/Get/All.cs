7ed505ecd35d56df3faea9e2f8db5ba21ecdd64a
julius-boettger <julius.btg@proton.me>
2024-06-11 12:11:51 +0200

    fix: override generic swagger doc for city feature

--- Voycar.Api.Web/Features/Cities/Endpoints/Get/All.cs
+++ Voycar.Api.Web/Features/Cities/Endpoints/Get/All.cs
@@ -7,4 +7,11 @@ public class All : Generic.Endpoint.Get.All<City>
 {
     // ToDo roles
     public All(ICities repository) : base(repository, ["admin"]) {}
+
+    public override void Configure()
+    {
+        base.Configure();
+        // Manually correct grammar
+        this.Summary(s => s.Summary = $"Retrieve all Cities");
+    }
 }
