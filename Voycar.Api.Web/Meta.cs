f4dc6c7ebd4f51b153c966750eed8b1a1b31b566
kenoalpe <git@kenoalbers.de>
2024-06-12 17:39:43 +0200

    feat: add FastEndpoints.Security to Meta.cs

--- Voycar.Api.Web/Meta.cs
+++ Voycar.Api.Web/Meta.cs
@@ -1,6 +1,12 @@
+// Logging
 global using Serilog;
+
+// API Framework
 global using FastEndpoints;
 global using FastEndpoints.Swagger;
+global using FastEndpoints.Security;
+
+// API helper
 global using FluentValidation;
 global using Microsoft.AspNetCore.Http.HttpResults;
 
