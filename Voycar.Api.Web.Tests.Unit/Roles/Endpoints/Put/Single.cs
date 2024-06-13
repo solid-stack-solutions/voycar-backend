e0a7508633745fb4477e83bb691fe8ddf8151465
kevin-lingk <klingk.msg@gmail.com>
2024-06-13 10:35:01 +0200

    test: verify failed update of role endpoint

--- Voycar.Api.Web.Tests.Unit/Roles/Endpoints/Put/Single.cs
+++ Voycar.Api.Web.Tests.Unit/Roles/Endpoints/Put/Single.cs
@@ -31,4 +31,27 @@ public class Single : TestBase<App>
         Assert.NotNull(rsp);
         Assert.Equal(StatusCodes.Status200OK, rsp.StatusCode);
     }
+
+
+    [Fact]
+    public async Task UpdateRoleFailure()
+    {
+        // Arrange
+        var fakeRole = new Role { Id = new Guid("F2E3156F-BC43-45F5-B8EE-024743E8BD2A"), Name = "fakeRole" };
+        var fakeRoleRepository = A.Fake<IRoles>();
+        A.CallTo(() => fakeRoleRepository.Update(fakeRole)).Returns(false);
+
+        var ep = Factory.Create<Features.Roles.Endpoints.Put.Single>(fakeRoleRepository);
+        fakeRole.Name = "newRoleName";
+        var req = fakeRole;
+
+        // Act
+        await ep.HandleAsync(req, default);
+        var rsp = ep.HttpContext.Response;
+
+        // Assert
+        A.CallTo(() => fakeRoleRepository.Update(fakeRole)).MustHaveHappenedOnceExactly();
+        Assert.NotNull(rsp);
+        Assert.Equal(StatusCodes.Status404NotFound, rsp.StatusCode);
+    }
 }
