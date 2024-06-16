ec026e9714e784890ffb6cc3a9ac56a3ea5903a6
NilsPvR <nilspvr@pm.me>
2024-06-16 16:33:13 +0200

    feat: add equals and to hashcode for entity

--- Voycar.Api.Web/Generic/Entity.cs
+++ Voycar.Api.Web/Generic/Entity.cs
@@ -9,4 +9,21 @@ public class Entity
 {
     [Key]
     public Guid Id { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        // Check if this ID is just the default ID
+        if (obj == null || this.GetType() != obj.GetType() || this.Id.Equals(new Guid()))
+        {
+            return false;
+        }
+
+        var other = (Entity)obj;
+        return this.Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.GetType().Name, this.Id);
+    }
 }
