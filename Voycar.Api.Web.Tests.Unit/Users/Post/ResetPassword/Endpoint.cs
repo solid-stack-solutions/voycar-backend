fbd44a2e40cfef3ae045205f8fdbe4f8e6f9e8e3
kevin-lingk <klingk.msg@gmail.com>
2024-06-17 20:47:50 +0200

    test: add test to verify failure of password reset if reset token is invalid

--- Voycar.Api.Web.Tests.Unit/Users/Post/ResetPassword/Endpoint.cs
+++ Voycar.Api.Web.Tests.Unit/Users/Post/ResetPassword/Endpoint.cs
@@ -79,4 +79,42 @@ public class Endpoint : TestBase<App>
         Assert.NotNull(rsp);
         Assert.Equal(StatusCodes.Status400BadRequest, rsp.StatusCode);
     }
+
+
+    [Fact]
+    public async Task ResetPasswordFailure_ResetResetToken()
+    {
+        // Arrange
+        var fakeUserRepository = A.Fake<IUsers>();
+        var fakeLogger = A.Fake<ILogger<Post.Register.Endpoint>>();
+
+        var req = new Request{PasswordResetToken = "resetToken", Password = "newPassword"};
+        var user = new User
+        {
+            Id = new Guid("47EB02FB-AA5B-4769-8B34-D23EC48DE506"),
+            Email = "test@test.de",
+            PasswordHash = "passwordHash",
+            PasswordResetToken = "resetToken",
+            ResetTokenExpires = new DateTime(2023,12,12)
+        };
+
+        var ep = Factory.Create<Features.Users.Endpoints.Post.ResetPassword.Endpoint>(ctx =>
+        {
+            ctx.AddTestServices(s =>
+            {
+                s.AddSingleton(fakeUserRepository);
+                s.AddSingleton(fakeLogger);
+            });
+        });
+
+        A.CallTo(() => fakeUserRepository.RetrieveByPasswordResetToken(req.PasswordResetToken)).Returns(user);
+
+        // Act
+        await ep.HandleAsync(req, default);
+        var rsp = ep.HttpContext.Response;
+
+        // Assert
+        Assert.NotNull(rsp);
+        Assert.Equal(StatusCodes.Status400BadRequest, rsp.StatusCode);
+    }
 }
