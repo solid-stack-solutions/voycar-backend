218fab7a455896719ac8b3aa24c3ec8162537153
kevin-lingk <klingk.msg@gmail.com>
2024-06-18 16:16:08 +0200

    refac: change request

--- Voycar.Api.Web.Tests.Unit/Users/Post/Register/Endpoint.cs
+++ Voycar.Api.Web.Tests.Unit/Users/Post/Register/Endpoint.cs
@@ -19,6 +19,12 @@ public class Endpoint : TestBase<App>
     private readonly IEmailService fakeEmailService = A.Fake<IEmailService>();
     private readonly ILogger fakeLogger = A.Fake<ILogger<Endpoint>>();
 
+    private readonly Request request = new()
+    {
+        Email = "test@test.de",
+        Password = "notsafe987",
+        BirthDate = new DateOnly(2000, 12, 12),
+    };
 
     private Features.Users.Endpoints.Post.Register.Endpoint SetupEndpoint()
     {
@@ -41,50 +47,20 @@ public class Endpoint : TestBase<App>
     {
         // Arrange
         var fakeRole = new Role { Id = new Guid("4ECB35CC-906C-46D7-AB3B-EDB468E1DD51"), Name = "member" };
-        var req = new Request
-        {
-            Email = "test@test.de",
-            Password = "notsafe987",
-            FirstName = "testFName",
-            LastName = "testLName",
-            Street = "test",
-            HouseNumber = "test",
-            PostalCode = "test",
-            City = "test",
-            Country = "test",
-            BirthDate = new DateOnly(2000, 12, 12),
-            BirthPlace = "test",
-            PhoneNumber = "test"
-        };
-
-        var mem = new Member
-        {
-            Id = new Guid("E5180CD2-A399-4EE3-AAE3-D909FA25AFF3"),
-            FirstName = "testFName",
-            LastName = "testLName",
-            Street = "test",
-            HouseNumber = "test",
-            PostalCode = "test",
-            City = "test",
-            Country = "test",
-            BirthDate = new DateOnly(2000, 12, 12),
-            BirthPlace = "test",
-            PhoneNumber = "test"
-        };
 
         var ep = this.SetupEndpoint();
 
-        var member = ep.Map.ToEntity(req);
-        var user = new User { Email = req.Email, PasswordHash = "hashedPassword" };
+        var member = ep.Map.ToEntity(this.request);
+        var user = new User { Email = this.request.Email, PasswordHash = "hashedPassword" };
 
-        A.CallTo(() => this.fakeUserRepository.RetrieveByEmail(req.Email)).Returns((User?)null);
+        A.CallTo(() => this.fakeUserRepository.RetrieveByEmail(this.request.Email)).Returns((User?)null);
         A.CallTo(() => this.fakeRoleRepository.Retrieve(fakeRole.Name)).Returns(fakeRole);
         A.CallTo(() => this.fakeMemberRepository.Create(member)).Returns(member.Id);
         A.CallTo(() => this.fakeUserRepository.Create(user)).Returns(user.Id);
         A.CallTo(() => this.fakeEmailService.SendVerificationEmail(user)).DoesNothing();
 
         // Act
-        await ep.HandleAsync(req, default);
+        await ep.HandleAsync(this.request, default);
         var rsp = ep.HttpContext.Response;
 
         // Assert
@@ -97,29 +73,13 @@ public class Endpoint : TestBase<App>
     public async Task Register_ExistingUser_Returns_BadRequest()
     {
         // Arrange
-        var req = new Request
-        {
-            Email = "test@test.de",
-            Password = "notsafe987",
-            FirstName = "testFName",
-            LastName = "testLName",
-            Street = "test",
-            HouseNumber = "test",
-            PostalCode = "test",
-            City = "test",
-            Country = "test",
-            BirthDate = new DateOnly(2000, 12, 12),
-            BirthPlace = "test",
-            PhoneNumber = "test"
-        };
-
         var ep = this.SetupEndpoint();
-        var user = new User { Email = req.Email, PasswordHash = "hashedPassword" };
+        var user = new User { Email = this.request.Email, PasswordHash = "hashedPassword" };
 
-        A.CallTo(() => this.fakeUserRepository.RetrieveByEmail(req.Email)).Returns(user);
+        A.CallTo(() => this.fakeUserRepository.RetrieveByEmail(this.request.Email)).Returns(user);
 
         // Act
-        await ep.HandleAsync(req, default);
+        await ep.HandleAsync(this.request, default);
         var rsp = ep.HttpContext.Response;
 
         // Assert
