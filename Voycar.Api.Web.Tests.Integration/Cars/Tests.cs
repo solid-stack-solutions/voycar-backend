e6fa291526ae5317defbbf65a88b5e082fc9adf9
julius-boettger <julius.btg@proton.me>
2024-06-19 16:52:09 +0200

    test: available cars with reservation

--- Voycar.Api.Web.Tests.Integration/Cars/Tests.cs
+++ Voycar.Api.Web.Tests.Integration/Cars/Tests.cs
@@ -109,4 +109,40 @@ public class Tests : TestBase<App, State>
         httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         response.Should().BeEquivalentTo(this.Context.Cars);
     }
+
+    [Fact]
+    public async Task Get_Available_ReturnsOkAndAvailableCars()
+    {
+        // Arrange
+        var requestData = ConstructAvailableRequest(
+            this._state.StationId,
+            "2000-01-01T08:00:00.000Z",
+            "2000-01-01T18:00:00.000Z"
+        );
+        // Pick random car to reserve
+        var reservedCar = this.Context.Cars.First();
+        var reservation = new Reservation
+        {
+            Id = Guid.NewGuid(),
+            Begin = DateTime.Parse("2000-01-01T10:00:00.000Z", CultureInfo.InvariantCulture).ToUniversalTime(),
+            End = DateTime.Parse("2000-01-01T12:00:00.000Z", CultureInfo.InvariantCulture).ToUniversalTime(),
+            MemberId = this.Context.Members.First().Id,
+            CarId = reservedCar.Id
+        };
+        this.Context.Reservations.Add(reservation);
+        await this.Context.SaveChangesAsync();
+
+        // Act
+        var (httpResponse, response) = await this._app.Admin.GETAsync<C.Get.Available.Endpoint, C.Get.Available.Request, IEnumerable<Car>>(requestData);
+
+        // Assert
+        this.Context.Reservations.Should().HaveCount(1, "Reservation table should contain only the one created in this test");
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Should().BeEquivalentTo(this.Context.Cars.Where(car => car.Id != reservedCar.Id));
+
+        // Cleanup
+        this.Context.Reservations.Remove(reservation);
+        await this.Context.SaveChangesAsync();
+        this.Context.Reservations.Should().BeEmpty();
+    }
 }
