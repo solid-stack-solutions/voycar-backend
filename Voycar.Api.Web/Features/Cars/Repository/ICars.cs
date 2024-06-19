e6a0325e3e5d68ce0ac25612a59e68d4ef847619
julius-boettger <julius.btg@proton.me>
2024-06-19 21:40:35 +0200

    doc: add comment to RetrieveAvailable

--- Voycar.Api.Web/Features/Cars/Repository/ICars.cs
+++ Voycar.Api.Web/Features/Cars/Repository/ICars.cs
@@ -4,5 +4,10 @@ using Entities;
 
 public interface ICars : Generic.Repository.IRepository<Car>
 {
+    /// <summary>
+    /// Retrieve all <see cref="Car"/>s at the given <see cref="Station"/>
+    /// that are not reserved (appear in no <see cref="Reservation"/>) during the whole given timespan.
+    /// Assumes that <c>stationId</c> is valid and time between <c>begin</c> and <c>end</c> is positive.
+    /// </summary>
     IEnumerable<Car> RetrieveAvailable(Guid stationId, DateTime begin, DateTime end);
 }
