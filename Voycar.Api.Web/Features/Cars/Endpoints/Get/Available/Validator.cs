780b0c9eea02b07957153979ff9f2ecf4a0157e7
julius-boettger <julius.btg@proton.me>
2024-06-19 14:14:21 +0200

    fix: available cars validator dependency injection

--- Voycar.Api.Web/Features/Cars/Endpoints/Get/Available/Validator.cs
+++ Voycar.Api.Web/Features/Cars/Endpoints/Get/Available/Validator.cs
@@ -4,11 +4,13 @@ using Stations.Repository;
 
 public class Validator : Validator<Request>
 {
-    public Validator(IStations stationRepository)
+    public Validator()
     {
-        this.RuleFor(req => stationRepository.Retrieve(req.StationId))
-            .NotNull().WithMessage("there is no station with such id");
         this.RuleFor(req => req.End.Subtract(req.Begin))
-            .GreaterThan(TimeSpan.Zero).WithMessage("end is not after begin");
+            .GreaterThan(TimeSpan.Zero)
+            .WithMessage("End-time is not after begin-time");
+        this.RuleFor(req => this.Resolve<IStations>().Retrieve(req.StationId))
+            .NotNull()
+            .WithMessage("There is no station with the given ID");
     }
 }
