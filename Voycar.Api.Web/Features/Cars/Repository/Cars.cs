287c7e65accc67f93d7e62d22f9e1fd3139f51cf
julius-boettger <julius.btg@proton.me>
2024-06-19 21:41:27 +0200

    feat: implement retrieving available cars

--- Voycar.Api.Web/Features/Cars/Repository/Cars.cs
+++ Voycar.Api.Web/Features/Cars/Repository/Cars.cs
@@ -14,8 +14,22 @@ public class Cars : Generic.Repository.Repository<Car>, ICars
 
     public IEnumerable<Car> RetrieveAvailable(Guid stationId, DateTime begin, DateTime end)
     {
-        this._logger.LogDebug($"Timespan: {end - begin}");
-        // ToDo
-        return this.RetrieveAll();
+        // Cars at given station
+        var availableCars = this.RetrieveAll()
+            .Where(car => car.StationId == stationId);
+
+        // Reservations concerning availableCars conflicting with the given timespan
+        var conflictingReservations = this._context.Reservations.ToArray()
+            .Where(res => availableCars.Contains(res.Car))
+            .Where(res =>
+                // res.Begin is in given timespan
+                (res.Begin >= begin && res.Begin <  end) ||
+                // res.End is in given timespan
+                (res.End   >  begin && res.End   <= end) ||
+                // Whole given timespan overlaps with reservation
+                (res.Begin <  begin && res.End   >  end));
+
+        // availableCars with no conflicting reservations
+        return availableCars.Where(car => conflictingReservations.All(res => car.Id != res.CarId));
     }
 }
