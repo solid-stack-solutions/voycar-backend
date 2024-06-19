d9939e3924ae90652cf0df6cf66cfe8003acecd5
julius-boettger <julius.btg@proton.me>
2024-06-19 13:52:05 +0200

    feat: add validator for available cars

---
+++ Voycar.Api.Web/Features/Cars/Endpoints/Get/Available/Validator.cs
@@ -0,0 +1,14 @@
+﻿namespace Voycar.Api.Web.Features.Cars.Endpoints.Get.Available;
+
+using Stations.Repository;
+
+public class Validator : Validator<Request>
+{
+    public Validator(IStations stationRepository)
+    {
+        this.RuleFor(req => stationRepository.Retrieve(req.StationId))
+            .NotNull().WithMessage("there is no station with such id");
+        this.RuleFor(req => req.End.Subtract(req.Begin))
+            .GreaterThan(TimeSpan.Zero).WithMessage("end is not after begin");
+    }
+}
