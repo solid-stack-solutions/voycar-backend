c3e5d97cdb8bffa9aaac646f0189d589b026426e
Kevin <136514166+kvnlnk@users.noreply.github.com>
2024-06-21 13:30:53 +0200

    refac: apply suggestion
    Co-authored-by: Nils van Rijsinge <nilspvr@pm.me>

--- Voycar.Api.Web/Features/Members/Endpoints/Get/Personal/Response.cs
+++ Voycar.Api.Web/Features/Members/Endpoints/Get/Personal/Response.cs
@@ -14,8 +14,8 @@ public class Response
     public string BirthPlace { get; set; }
 
     // Contact information
-    public string Email { get; set; }
     public string PhoneNumber { get; set; }
+    public string Email { get; set; }
 
     // Plan information
     public string PlanName { get; set; }
