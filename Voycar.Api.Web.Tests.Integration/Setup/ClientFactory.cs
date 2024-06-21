d89d8c238712d1b1119e82cb6fc1d3048eb9c676
kevin-lingk <klingk.msg@gmail.com>
2024-06-21 13:15:02 +0200

    style: reformat user entity creation

--- Voycar.Api.Web.Tests.Integration/Setup/ClientFactory.cs
+++ Voycar.Api.Web.Tests.Integration/Setup/ClientFactory.cs
@@ -136,7 +136,9 @@ public static class ClientFactory
         // Create user entity
         var user = new User()
         {
-            Email = email, PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password), RoleId = roleId
+            Email = email,
+            PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password),
+            RoleId = roleId
         };
         context.Users.Add(user);
         var changedAmount = await context.SaveChangesAsync();
