298bbdb87d0818222ff963ad09eeb6eda30dd861
julius-boettger <julius.btg@proton.me>
2024-06-22 18:18:05 +0200

    feat: implement creating reservations after checking for conflicts

--- Voycar.Api.Web/Features/Reservations/Repository/Reservations.cs
+++ Voycar.Api.Web/Features/Reservations/Repository/Reservations.cs
@@ -2,6 +2,7 @@ namespace Voycar.Api.Web.Features.Reservations.Repository;
 
 using Context;
 using Entities;
+using Microsoft.IdentityModel.Tokens;
 
 public class Reservations : Generic.Repository.Repository<Reservation>, IReservations
 {
@@ -9,6 +10,31 @@ public class Reservations : Generic.Repository.Repository<Reservation>, IReserva
 
     public Guid? Create(Guid carId, Guid memberId, DateTime begin, DateTime end)
     {
-        return null;
+        var noConflicts = this.DbSet
+            // Reservations for the same car
+            .Where(res => res.CarId == carId)
+            // Reservations with conflicting timespans
+            .Where(res =>
+                // res.Begin is in given timespan
+                (res.Begin >= begin && res.Begin < end) ||
+                // res.End is in given timespan
+                (res.End > begin && res.End <= end) ||
+                // Whole given timespan overlaps with reservation
+                (res.Begin < begin && res.End > end))
+            .IsNullOrEmpty();
+
+        if (!noConflicts)
+        {
+            return null;
+        }
+
+        return this.Create(new Reservation
+        {
+            Id = Guid.NewGuid(),
+            Begin = begin,
+            End = end,
+            CarId = carId,
+            MemberId = memberId
+        });
     }
 }
