a51a20ba7845abe661e16d4415e27a8e66eb4761
kevin-lingk <klingk.msg@gmail.com>
2024-06-22 15:17:55 +0200

    feat: add Mapper.cs

---
+++ Voycar.Api.Web/Features/Members/Endpoints/Put/Personal/Mapper.cs
@@ -0,0 +1,27 @@
+namespace Voycar.Api.Web.Features.Members.Endpoints.Put.Personal;
+
+using Entities;
+
+
+public class Mapper : ResponseMapper<Request, Member>
+{
+    public Member ToEntity(Request r, Member member)
+    {
+        // Create the Member entity
+        return new Member
+        {
+            Id = member.Id,
+            FirstName = r.FirstName,
+            LastName = r.LastName,
+            Street = r.Street,
+            HouseNumber = r.HouseNumber,
+            PostalCode = r.PostalCode,
+            City = r.City,
+            Country = r.Country,
+            BirthDate = r.BirthDate,
+            BirthPlace = r.BirthPlace,
+            PhoneNumber = r.PhoneNumber,
+            PlanId = member.PlanId
+        };
+    }
+}
