09c866d1d7cf440bc81ba693527090b8f0f222cc
kevin-lingk <klingk.msg@gmail.com>
2024-06-22 21:08:30 +0200

    fix: correct spelling

--- Voycar.Api.Web/Features/Users/Endpoints/Post/Register/Validator.cs
+++ Voycar.Api.Web/Features/Users/Endpoints/Post/Register/Validator.cs
@@ -11,44 +11,44 @@ public class Validator : Validator<Request>
     public Validator()
     {
         this.RuleFor(request => request.Email)
-            .NotEmpty().WithMessage("your email is required!")
-            .EmailAddress().WithMessage("not a valid email address");
+            .NotEmpty().WithMessage("Your email is required!")
+            .EmailAddress().WithMessage("Not a valid email address");
 
         this.RuleFor(request => request.Password)
-            .NotEmpty().WithMessage("your password is required!");
+            .NotEmpty().WithMessage("Your password is required!");
 
         this.RuleFor(request => request.FirstName)
-            .NotEmpty().WithMessage("your firstname is required!")
-            .Length(2, 250).WithMessage("your name is too short or too long!");
+            .NotEmpty().WithMessage("Your firstname is required!")
+            .Length(2, 250).WithMessage("Your name is too short or too long!");
 
         this.RuleFor(request => request.LastName)
-            .NotEmpty().WithMessage("your firstname is required!")
-            .Length(2, 250).WithMessage("your name is too short or too long!");
+            .NotEmpty().WithMessage("Your firstname is required!")
+            .Length(2, 250).WithMessage("Your name is too short or too long!");
 
         this.RuleFor(request => request.Street)
-            .NotEmpty().WithMessage("your street is required!");
+            .NotEmpty().WithMessage("Your street is required!");
 
         this.RuleFor(request => request.HouseNumber)
-            .NotEmpty().WithMessage("your house number is required!");
+            .NotEmpty().WithMessage("Your house number is required!");
 
         this.RuleFor(request => request.PostalCode)
-            .NotEmpty().WithMessage("your postal code is required!");
+            .NotEmpty().WithMessage("Your postal code is required!");
 
         this.RuleFor(request => request.City)
-            .NotEmpty().WithMessage("your place is required!");
+            .NotEmpty().WithMessage("Your place is required!");
 
         this.RuleFor(request => request.Country)
-            .NotEmpty().WithMessage("your country is required!");
+            .NotEmpty().WithMessage("Your country is required!");
 
         this.RuleFor(request
                 => (DateOnly.FromDateTime(DateTime.UtcNow).DayNumber
                     - request.BirthDate.DayNumber) / 365)
-            .GreaterThan(18).WithMessage("you are not legal yet!");
+            .GreaterThan(18).WithMessage("You are not legal yet!");
 
         this.RuleFor(request => request.BirthPlace)
-            .NotEmpty().WithMessage("your birth place is required!");
+            .NotEmpty().WithMessage("Your birth place is required!");
 
         this.RuleFor(request => request.PhoneNumber)
-            .NotEmpty().WithMessage("your phone number is required!");
+            .NotEmpty().WithMessage("Your phone number is required!");
     }
 }
