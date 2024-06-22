03ace1d88a7af0f6e3f51bdb960b1ef96cc10e01
Kevin <136514166+kvnlnk@users.noreply.github.com>
2024-06-22 12:16:32 +0200

    doc: apply suggestion
    Co-authored-by: julius-boettger <julius.btg@proton.me>

--- Voycar.Api.Web/Features/Reservation/Endpoints/Get/Personal/Endpoint.cs
+++ Voycar.Api.Web/Features/Reservation/Endpoints/Get/Personal/Endpoint.cs
@@ -24,8 +24,8 @@ public class Endpoint : Endpoint<Request, Results<Ok<Response>, BadRequest<strin
         this.Roles("member");
         this.Summary(summary =>
         {
-            summary.Summary = "Get reservations of logged in member";
-            summary.Description = "Get the reservations of the member who is logged in according to the request cookie";
+            summary.Summary = $"Get {nameof(Reservation)}s of logged in member";
+            summary.Description = $"Get the {nameof(Reservation)}s of the member who is logged in according to the request cookie";
             summary.Responses[200] = "If member is found";
             summary.Responses[400] = "If the user somehow can't be found or the user is not a member.";
         });
