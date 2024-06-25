34c36546733fde3aa38524a19ca9dc13fde19b8c
kevin-lingk <klingk.msg@gmail.com>
2024-06-25 17:54:15 +0200

    test: add unittest to test error handling in Endpoint.cs of Put.Personal

---
+++ Voycar.Api.Web.Tests.Unit/Members/Endpoints/Put/Personal/Endpoint.cs
@@ -0,0 +1,144 @@
+namespace Voycar.Api.Web.Tests.Unit.Members.Endpoints.Put.Personal;
+
+using Entities;
+using FakeItEasy;
+using Features.Members.Endpoints.Put.Personal;
+using Features.Members.Repository;
+using Features.Users.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+public class Endpoint : TestBase<App>
+{
+    private readonly IUsers FakeUserRepository = A.Fake<IUsers>();
+    private readonly IMembers FakeMemberRepository = A.Fake<IMembers>();
+
+
+    private readonly Request Request = new()
+    {
+        FirstName = "null",
+        LastName = "null",
+        Street = "null",
+        HouseNumber = "null",
+        PostalCode = "null",
+        City = "null",
+        Country = "null",
+        BirthDate = new DateOnly(2002, 06, 25),
+        BirthPlace = "null",
+        PhoneNumber = "null",
+    };
+
+
+    private Features.Members.Endpoints.Put.Personal.Endpoint SetupEndpoint()
+    {
+        return Factory.Create<Features.Members.Endpoints.Put.Personal.Endpoint>(ctx =>
+        {
+            ctx.AddTestServices(s =>
+            {
+                s.AddSingleton(this.FakeUserRepository);
+                s.AddSingleton(this.FakeMemberRepository);
+            });
+        });
+    }
+
+
+    [Fact]
+    public async Task Put_Request_ReturnsBadRequest_DueToNullUser()
+    {
+        // Arrange
+        var ep = this.SetupEndpoint();
+
+        A.CallTo(() => this.FakeUserRepository.Retrieve(this.Request.UserId)).Returns(null);
+
+        // Act
+        async Task Act() => await ep.HandleAsync(this.Request, default);
+
+
+        // Assert
+        var exception = await Assert.ThrowsAnyAsync<ValidationFailureException>(Act);
+        Assert.NotNull(exception);
+        Assert.Equal("ThrowError() called! - User does not exist", exception.Message);
+    }
+
+
+    [Fact]
+    public async Task Put_Request_ReturnsBadRequest_DueToNullMember()
+    {
+        // Arrange
+        var ep = this.SetupEndpoint();
+        var user = new User
+        {
+            Email = "test@test.de",
+            PasswordHash = "notsafe987",
+            MemberId = null
+        };
+
+
+        A.CallTo(() => this.FakeUserRepository.Retrieve(this.Request.UserId)).Returns(user);
+
+        // Act
+        async Task Act() => await ep.HandleAsync(this.Request, default);
+
+        // Assert
+        var exception = await Assert.ThrowsAnyAsync<ValidationFailureException>(Act);
+        Assert.NotNull(exception);
+        Assert.Equal("ThrowError() called! - User is not a member", exception.Message);
+    }
+
+    [Fact]
+    public async Task Put_Request_ReturnsBadRequest_DueToNonExistingMember()
+    {
+        // Arrange
+        var ep = this.SetupEndpoint();
+        var user = new User
+        {
+            Email = "test@test.de",
+            PasswordHash = "notsafe987",
+            MemberId = new Guid("E610101A-ED58-4A2F-8DEB-9FEAAEDD6B7C")
+        };
+
+
+        A.CallTo(() => this.FakeUserRepository.Retrieve(this.Request.UserId)).Returns(user);
+        A.CallTo(() => this.FakeMemberRepository.Retrieve((Guid)user.MemberId)).Returns(null);
+
+        // Act
+        async Task Act() => await ep.HandleAsync(this.Request, default);
+
+        // Assert
+        var exception = await Assert.ThrowsAnyAsync<ValidationFailureException>(Act);
+        Assert.NotNull(exception);
+        Assert.Equal("ThrowError() called! - Member does not exist", exception.Message);
+    }
+
+    [Fact]
+    public async Task Put_Request_ReturnsBadRequest_DueToFailedUpdate()
+    {
+        // Arrange
+        var ep = this.SetupEndpoint();
+        var user = new User
+        {
+            Email = "test@test.de",
+            PasswordHash = "notsafe987",
+            MemberId = new Guid("E610101A-ED58-4A2F-8DEB-9FEAAEDD6B7C")
+        };
+
+        var member = new Member
+        {
+            Id = new Guid("E610101A-ED58-4A2F-8DEB-9FEAAEDD6B7C"),
+        };
+
+
+        A.CallTo(() => this.FakeUserRepository.Retrieve(this.Request.UserId)).Returns(user);
+        A.CallTo(() => this.FakeMemberRepository.Retrieve((Guid)user.MemberId)).Returns(member);
+        A.CallTo(() => this.FakeMemberRepository.Update(member)).Returns(false);
+
+        // Act
+        async Task Act() => await ep.HandleAsync(this.Request, default);
+
+        // Assert
+        var exception = await Assert.ThrowsAnyAsync<ValidationFailureException>(Act);
+        Assert.NotNull(exception);
+        Assert.Equal("ThrowError() called! - Failed to update member data", exception.Message);
+    }
+}
